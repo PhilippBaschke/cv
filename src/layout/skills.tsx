b5b3ebe0275817281480edf594fd37b27902db47
Philipp Baschke <philipp@baschke.com>
2022-03-15 14:06:14 +0100

    Style the skills section
    I used a more tag cloud like design before but it lead to more trouble
    with copying the text than the underlined design does (due to horizontal
    padding). This solution can still cause wrong copy behavior but it's
    less likely.

--- src/layout/skills.tsx
+++ src/layout/skills.tsx
@@ -1,13 +1,23 @@
 import type { SkillsData } from '../data-types/skills-data';
 import { SectionTitle } from './components/section-title';
-import { Text, View } from '@react-pdf/renderer';
+import { SkillsList } from './components/skills-list';
+import { space } from './tokens';
+import { StyleSheet, View } from '@react-pdf/renderer';
 import React from 'react';
+
+const styles = StyleSheet.create({
+  spacedList: { marginBottom: space[2] },
+});
 
 const Skills = ({ familiar, proficient }: SkillsData) => (
   <View>
     <SectionTitle>Skills</SectionTitle>
-    <Text>Proficient: {proficient.join(', ')}</Text>
-    <Text>Familiar: {familiar.join(', ')}</Text>
+    <SkillsList
+      skills={proficient}
+      style={styles.spacedList}
+      title="Proficient"
+    />
+    <SkillsList skills={familiar} title="Familiar" />
   </View>
 );
 
